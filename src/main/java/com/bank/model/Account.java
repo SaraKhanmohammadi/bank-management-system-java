0fa6915d54991e58d6cfdca5252fb7c1961c05e2
sara <sara.khanmohamadi13717@gmail.com>
2026-06-14 15:29:15 +0330

    Implement Account model

--- src/main/java/com/bank/model/Account.java
+++ src/main/java/com/bank/model/Account.java
@@ -1,4 +1,41 @@
 package com.bank.model;
 
 public class Account {
+    private String accountNumber;
+    private  double balance;
+    private  User owner;
+
+
+    public Account(String accountNumber, User owner){
+
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = 0;
+    }
+
+
+    public  String getAccountNumber(){
+        return accountNumber;
+
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public User getOwner(){
+        return  owner;
+    }
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public  void withdraw(double amount){
+        if (amount > balance){
+            System.out.println("Insufficient balance");
+            return;
+        }
+        balance -= amount;
+    }
 }
