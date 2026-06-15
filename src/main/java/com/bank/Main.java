e21422ae22ab1f16971e50b9715ec36b62337661
sara <sara.khanmohamadi13717@gmail.com>
2026-06-15 12:15:20 +0330

    Implement money transfer feature

--- src/main/java/com/bank/Main.java
+++ src/main/java/com/bank/Main.java
@@ -8,20 +8,26 @@ public class Main {
     public  static  void main(String[] args){
 
         User user = new User("Sara", "1234567890" );
-        Account account = new Account("Acc1001", user);
+        Account account = new Account("ACC1001", user);
+        User secondUser= new User("Hamid", "9876543210");
+        Account secondAccount = new Account("ACC2001", secondUser);
         BankService bankService = new BankService();
         bankService.createAccount(account);
+        bankService.createAccount(secondAccount);
         account.deposit(500);
         account.withdraw(200);
         System.out.println("Current Balance: " + account.getBalance());
+
 
 
         Account foundAccount = bankService.findAccount("ACC1001");
         if (foundAccount != null){
             System.out.println("Account found: "+ foundAccount.getAccountNumber());
         }
+        bankService.transfer("ACC1001", "ACC2001", 100);
+        System.out.println("First Account Balance: " + account.getBalance());
 
-
+        System.out.println("Second Account Balance: " + secondAccount.getBalance());
     }
 
 }
--- src/main/java/com/bank/service/BankService.java
+++ src/main/java/com/bank/service/BankService.java
@@ -4,20 +4,35 @@ import java.util.ArrayList;
 
 public class BankService {
 
-    private  ArrayList<Account> accounts = new ArrayList<>();
+    private ArrayList<Account> accounts = new ArrayList<>();
 
-    public void createAccount (Account account){
+    public void createAccount(Account account) {
         accounts.add(account);
         System.out.println("Account created successfully");
     }
 
 
-    public  Account findAccount(String accountNumber){
-        for (Account account : accounts){
-            if (account.getAccountNumber().equals(accountNumber)){
+    public Account findAccount(String accountNumber) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
                 return account;
             }
         }
-        return  null;
+        return null;
     }
+
+    public void transfer(String fromAccountNumber, String toAccountNumber, double amount) {
+        Account fromAccount = findAccount(fromAccountNumber);
+        Account toAccount = findAccount(toAccountNumber);
+
+        if (fromAccount == null || toAccount == null) {
+            System.out.println("Account not found");
+            return;
+        }
+
+        fromAccount.withdraw(amount);
+        toAccount.deposit(amount);
+        System.out.println("Transfer successful");
+    }
+
 }
